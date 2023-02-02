09835187dfb0e09b11f902f4f654795abb6dc1a4
tony-albanese <tony.albanese2000@gmail.com>
2023-02-02 09:45:10 +0000

    Add get method for getting a single profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -9,4 +10,11 @@ class ProfileView(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetailView(APIView):
+    def get(self, request, id):
+        profile = get_object_or_404(Profile, id=id)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
