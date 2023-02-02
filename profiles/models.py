14379169a7a161c07faf2b0806c940d8c1b1dfcc
tony-albanese <tony.albanese2000@gmail.com>
2023-02-02 08:22:39 +0000

    Add Profile model fields.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from kitchen_user import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    SPECIALTY_CHOICES = [
+        ('DES', 'Dessert'),
+        ('MEAT', 'Meats'),
+        ('GRILL', 'Grilling'),
+        ('VEG', 'Vegetables'),
+        ('BREAK', 'Breakfast'),
+        ('NO', 'Nothing Really'),
+        ('ALL', 'A little of everything')
+    ]
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    specialty = models.CharField(max_length=5, choices=SPECIALTY_CHOICES, default='NO')
+    avatar = models.ImageField(upload_to='images/', default='../default_avatar_kaw1ox')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
