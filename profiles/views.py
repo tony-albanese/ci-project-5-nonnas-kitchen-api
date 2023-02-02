44056994038bdb84fcc516d78d2204d7c659ccf2
tony-albanese <tony.albanese2000@gmail.com>
2023-02-02 10:07:44 +0000

    Add put method for updating a profile.

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -14,7 +15,19 @@ class ProfileView(APIView):
 
 
 class ProfileDetailView(APIView):
+    serializer_class = ProfileSerializer
+
+    
     def get(self, request, id):
         profile = get_object_or_404(Profile, id=id)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+    
+    def put(self, request, id):
+        profile = get_object_or_404(Profile, id=id)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
