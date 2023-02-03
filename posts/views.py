528783408cbfe6a02897ae2c53988cae9dfb39cc
tony-albanese <tony.albanese2000@gmail.com>
2023-02-03 13:49:01 +0000

    Add import statements  to views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import BlogPost
+from .serializers import BlogPostSerializer
 # Create your views here.
+
+
