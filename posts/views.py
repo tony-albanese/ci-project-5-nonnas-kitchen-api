90309086f558fc2ab5bbad0b852dcc2b3e84515f
tony-albanese <tony.albanese2000@gmail.com>
2023-02-03 15:34:53 +0000

    Define put and delete method for BlogPost

--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import BlogPost
 from .serializers import BlogPostSerializer
-from kitchen.permissions import OwnerPermissions
+from kitchen.permissions import AuthorPermissions
 # Create your views here.
 
 
@@ -32,7 +32,7 @@ class BlogPostView(APIView):
 
 
 class BlogPostDetailView(APIView):
-    permission_classes = [OwnerPermissions]
+    permission_classes = [AuthorPermissions]
     serializer_class = BlogPostSerializer
 
     def get(self, request, id):
@@ -41,3 +41,19 @@ class BlogPostDetailView(APIView):
             blog_post, context={'request': request}
         )
         return Response(serializer.data)
+    
+    def put(self, request, id):
+        blog_post = get_object_or_404(BlogPost, id=id)
+        serializer = BlogPostSerializer(
+            blog_post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        blog_post = get_object_or_404(BlogPost, id=id)
+        blog_post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
