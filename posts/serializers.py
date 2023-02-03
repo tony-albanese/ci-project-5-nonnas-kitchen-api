079fec4cfd86011fa3d9bf44486ea36694fcb46f
tony-albanese <tony.albanese2000@gmail.com>
2023-02-03 14:46:16 +0000

    Add image file and dimension size validation.

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,7 +9,18 @@ class BlogPostSerializer(TaggitSerializer, serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='author.profile.id')
     profile_image = serializers.ReadOnlyField(source='author.profile.avatar.url')
     tags = TagListSerializerField()
-    
+
+    def validate_post_image(self, value):
+        if value.size > 1024*1024*2:
+            raise serializers.ValidationError(
+                'Image is larger than 2MB.'
+            )
+        if value.image.width > 2048 or value.image.height > 2048:
+            raise serializers.ValidationError(
+                'Image dimension error.'
+            )
+        return value
+
     def get_is_author(self, obj):
         request = self.context['request']
         return request.user == obj.author
