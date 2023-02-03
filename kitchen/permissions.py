de890a98f5e29336ade67563403d83dfe340fb1a
tony-albanese <tony.albanese2000@gmail.com>
2023-02-03 15:34:14 +0000

    Add permission class for Authors

--- kitchen/permissions.py
+++ kitchen/permissions.py
@@ -7,4 +7,11 @@ class OwnerPermissions(permissions.BasePermission):
             return True
         else:
             return obj.owner == request.user
-        
+
+
+class AuthorPermissions(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj.author == request.user
