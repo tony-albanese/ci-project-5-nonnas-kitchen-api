ed188b24e99898b389cc0c7de2c2fedea24ee7a6
tony-albanese <tony.albanese2000@gmail.com>
2023-02-03 13:47:17 +0000

    Implement serializer for BlogPost

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,2 +1,22 @@
 from taggit.serializers import (TagListSerializerField, TaggitSerializer)
+from rest_framework import serializers
+from .models import BlogPost
 
+
+class BlogPostSerializer(TaggitSerializer, serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    is_author = tags = TagListSerializerField()
+    profile_id = serializers.ReadOnlyField(source='author.profile.id')
+    profile_image = serializers.ReadOnlyField(source='author.profile.avatar.url')
+    tags = TagListSerializerField()
+    
+    def get_is_author(self, obj):
+        request = self.context['request']
+        return request.user == obj.author
+    
+
+    class Meta:
+        model = BlogPost
+        fields = [
+            'id', 'author', 'title',  'body','tags', 'posted_on', 'post_image', 'is_owner','profile_id','profile_image',
+        ]
