dbdf36cea8395d931cd20c199bf84237b6f99a4e
tony-albanese <tony.albanese2000@gmail.com>
2023-02-03 14:32:42 +0000

    Add permission and post method for BlogPostView

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import BlogPost
@@ -8,6 +8,11 @@ from .serializers import BlogPostSerializer
 
 
 class BlogPostView(APIView):
+    serializer_class = BlogPostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         context = {'request': request}
         blog_posts = BlogPost.objects.all()
@@ -15,3 +20,11 @@ class BlogPostView(APIView):
 
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = BlogPostSerializer(data=request.data, context={'request': request})
+
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
