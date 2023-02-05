b7cd9a5c9faf08bcb8d6a1f6cd08b7d31426c5f9
tony-albanese <tony.albanese2000@gmail.com>
2023-02-05 21:48:25 +0000

    Add Like model. Comment out number_likes method

--- posts/models.py
+++ posts/models.py
@@ -17,7 +17,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author')
     body = models.TextField()
-    likes = models.ManyToManyField(User, blank=True, related_name='post_likes')
+    # likes = models.ManyToManyField(User, blank=True, related_name='post_likes')
     ratings = models.ManyToManyField(User, blank=True, related_name='post_ratings')
     posted_on = models.DateTimeField(auto_now=True)
     post_image = models.ImageField(upload_to='images/', default='../blogpost_default_image_v2nwpm')
@@ -31,8 +31,8 @@ class BlogPost(models.Model):
     def __str__(self):
         return self.title
 
-    def number_of_likes(self):
-        return self.likes.count()
+    # def number_of_likes(self):
+    #    return self.likes.count()
     
     def average_rating(self):
         return Rating.objects.filter(blog_post=self).aggregate(Avg("rating")["rating__avg"])
@@ -46,3 +46,14 @@ class Rating(models.Model):
     def __str__(self):
         return f"{self.blog_post.title}: {self.rating}"
 
+class Like(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='likes')
+    created_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        unique_together = ['owner', 'blog_post']
+
+    def __str__(self):
+        return f"Owner: {self.owner} Blog Post: {self.blog_post.title}"
