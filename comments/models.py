32d99c5a47201fbfa01a00d32889f792c82fc495
tony-albanese <tony.albanese2000@gmail.com>
2023-02-05 20:12:21 +0000

    Add Comment model.

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from kitchen_user.models import User
 # Create your models here.
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.body
+    
