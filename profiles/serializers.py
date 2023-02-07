cc5a82196cfd10ce2f5f425b9647002d7a12c132
tony-albanese <tony.albanese2000@gmail.com>
2023-02-07 15:54:48 +0000

    Add is_followed field to serializer.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(following=user, follower=obj.owner).first()
+            return following.id if following else None
+        return None
+
+
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'first_name', 'last_name', 'created_on', 'bio', 'avatar', 'is_owner'
+            'id', 'owner', 'first_name', 'last_name', 'created_on', 'bio', 'avatar', 'is_owner', 'following_id'
         ]
