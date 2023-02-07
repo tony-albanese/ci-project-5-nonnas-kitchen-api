9732aaedf175e9e42589bec34b5d4cd9eab8418e
tony-albanese <tony.albanese2000@gmail.com>
2023-02-07 10:32:42 +0000

    Refactored Comment to inherit from AbstractComment

--- comments/models.py
+++ comments/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 from posts.models import BlogPost
-from kitchen_user.models import User
+from base_models.models import AbsrtactComment
+
 # Create your models here.
 
-class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+class Comment(AbsrtactComment):
     blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add=True)
-    body = models.TextField()
     # TODO must implement likes
-
-
-    class Meta:
-        ordering = ['-created_on']
-
-    def __str__(self):
-        return self.body
     
