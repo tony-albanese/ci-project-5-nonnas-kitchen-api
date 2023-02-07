ed82987292d16695ec91e2492d61dcf69f5b4eae
tony-albanese <tony.albanese2000@gmail.com>
2023-02-07 13:56:50 +0000

    Add Follower model.

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from kitchen_user.models import User
 
 # Create your models here.
+
+
+class Follower(models.Model):
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
+    followed_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-followed_on']
+        unique_together = ['following', 'follower']
+
+    def __str__(self):
+        return f'{self.following} {self.follower}'
