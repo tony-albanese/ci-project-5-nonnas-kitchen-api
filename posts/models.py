8dd3806879b2328b3c4cb66a10dd965512f009e9
tony-albanese <tony.albanese2000@gmail.com>
2023-02-07 17:21:34 +0000

    Delete commented out code.

--- posts/models.py
+++ posts/models.py
@@ -18,22 +18,16 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author')
     body = models.TextField()
-    # likes = models.ManyToManyField(User, blank=True, related_name='post_likes')
     ratings = models.ManyToManyField(User, blank=True, related_name='post_ratings')
     posted_on = models.DateTimeField(auto_now=True)
     post_image = models.ImageField(upload_to='images/', default='../blogpost_default_image_v2nwpm')
     category = models.CharField(max_length=5, choices=CATEGORIES, default='anec')
-
-    # TODO: Add likes and ratings
 
     class Meta:
         ordering = ['-posted_on']
 
     def __str__(self):
         return self.title
-
-    # def number_of_likes(self):
-    #    return self.likes.count()
     
     def average_rating(self):
         return Rating.objects.filter(blog_post=self).aggregate(Avg("rating")["rating__avg"])
