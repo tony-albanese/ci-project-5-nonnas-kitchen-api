011986baacf496e4c308a5006afc28b322b45035
tony-albanese <tony.albanese2000@gmail.com>
2023-02-07 17:01:22 +0000

    Refactor BlogDetailView to inherit from generic view

--- posts/views.py
+++ posts/views.py
@@ -19,35 +19,10 @@ class BlogPostView(generics.ListCreateAPIView):
         serializer.save(author=self.request.user)
 
 
-class BlogPostDetailView(APIView):
+class BlogPostDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorPermissions]
     serializer_class = BlogPostSerializer
-
-    def get(self, request, id):
-        blog_post = get_object_or_404(BlogPost, id=id)
-        self.check_object_permissions(self.request, blog_post)
-        serializer = BlogPostSerializer(
-            blog_post, context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        blog_post = get_object_or_404(BlogPost, id=id)
-        self.check_object_permissions(self.request, blog_post)
-        serializer = BlogPostSerializer(
-            blog_post, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        blog_post = get_object_or_404(BlogPost, id=id)
-        self.check_object_permissions(self.request, blog_post)
-        blog_post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = BlogPost.objects.all()
 
 
 class LikeList(generics.ListCreateAPIView):
