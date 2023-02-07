c4dc89749ed2dd8336a9cfdb1fad3adfb890da6d
tony-albanese <tony.albanese2000@gmail.com>
2023-02-07 13:35:35 +0000

    Add class view for Follower list

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import FollowerSerializer
+from .models import Follower
 
 # Create your views here.
+
+
+class FollowerListView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(following=self.request.user)
