e3e09cc8ea6204d77ec6f9af831009d93a96aee0
tony-albanese <tony.albanese2000@gmail.com>
2023-02-07 10:36:29 +0000

    Fix typo in AbstractComment class name

--- base_models/models.py
+++ base_models/models.py
@@ -3,7 +3,7 @@ from kitchen_user.models import User
 # Create your models here.
 
 
-class AbsrtactComment(models.Model):
+class AbstractComment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
--- comments/models.py
+++ comments/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from posts.models import BlogPost
-from base_models.models import AbsrtactComment
+from base_models.models import AbstractComment
 
 # Create your models here.
 
-class Comment(AbsrtactComment):
+class Comment(AbstractComment):
     blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
     # TODO must implement likes
     
