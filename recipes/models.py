f18c22e629b8c25667c95b1bf99699541c032426
tony-albanese <tony.albanese2000@gmail.com>
2023-02-08 13:31:29 +0000

    Implement Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -6,18 +6,45 @@ from taggit.managers import TaggableManager
 
 
 class Recipe(models.Model):
+    DISH_TYPE = [
+        ('app','Appetizer'),
+        ('mains', 'Main Dish'),
+        ('pasta', 'Pasta Dish'),
+        ('meat', 'Meat Dish'),
+        ('poultry', 'Poultry Dish'),
+        ('vef', 'Vegetable Dish'),
+        ('rice', 'Rice Dish'),
+        ('pastry', 'Pastry'),
+        ('dessert', 'Dessert')
+    ]
+
+    DIFFICULTY = [
+        ('easy', 'Easy'),
+        ('medium', 'Medium'),
+        ('hard', 'Hard')
+    ]
+
+    TIME_UNITS = [
+        ('min', 'min'),
+        ('hr', 'hours')
+    ]
+
+    TAGS = [
+        'diabetic', 'gluten free', 'vegan', 'vegetarian', 'low calorie'
+    ]
+
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     description = models.TextField()
-    # type choice field
-    # meal category choice field
-    # difficulty choice field
-    # time
-    # time_unit
-    # yield
-    # ingredients list
-    # procedure
-    # tags : diabetic, gluten_free, vegan, vegetarian, low_calorie
+    dish_type = models.CharField(max_length=7, choices=DISH_TYPE, default='app')
+    difficulty = models.CharField(max_length=6, choices=DIFFICULTY, default='easy')
+    time = models.IntegerField(default=1)
+    time_unit = models.CharField(max_length=3, default='hr')
+    servings = models.IntegerField(default=1)
+    ingredients_list = models.JSONField()
+    procedure = models.JSONField()
+    tags = TaggableManager()
+    recipe_image = models.ImageField(upload_to='images/', default='../blogpost_default_image_v2nwpm')
 
     def __str__(self):
         return self.title
