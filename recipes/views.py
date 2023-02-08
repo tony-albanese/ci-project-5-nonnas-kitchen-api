b0e01f1018880f3695083fabb30bcbf031c97393
tony-albanese <tony.albanese2000@gmail.com>
2023-02-08 13:32:27 +0000

    Add import statements and class def for view

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.db.models import Count
+from rest_framework import permissions, generics, filters
+from .models import Recipe
+from .serializers import RecipeSerializer
+from kitchen.permissions import AuthorPermissions, OwnerPermissions
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
+
+
+class RecipeView(generics.ListCreateAPIView):
+    pass
