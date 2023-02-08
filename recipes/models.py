5160d737c24851ac28be0ba95d2134b068e9526a
tony-albanese <tony.albanese2000@gmail.com>
2023-02-08 12:58:49 +0000

    Add import statements.

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from kitchen_user.models import User
+from base_models.models import AbstractLike, AbstractRating
+from taggit.managers import TaggableManager
 # Create your models here.
+
+
+class Recipe(models.Model):
+    pass
