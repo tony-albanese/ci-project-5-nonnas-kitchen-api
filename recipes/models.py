7aadfc7931cee56d56189f1d2dcb0464ceb09c40
tony-albanese <tony.albanese2000@gmail.com>
2023-02-08 13:05:57 +0000

    Outline fields in Recipe model.

--- recipes/models.py
+++ recipes/models.py
@@ -6,4 +6,27 @@ from taggit.managers import TaggableManager
 
 
 class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    # type choice field
+    # meal category choice field
+    # difficulty choice field
+    # time
+    # time_unit
+    # yield
+    # ingredients list
+    # procedure
+    # tags : diabetic, gluten_free, vegan, vegetarian, low_calorie
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeLike(AbstractLike):
     pass
+
+
+class RecipeRating(AbstractRating):
+    pass
+
