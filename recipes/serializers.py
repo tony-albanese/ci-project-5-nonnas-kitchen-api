7d156c5d976a4727bfd89e57efdef19ee69b3d44
tony-albanese <tony.albanese2000@gmail.com>
2023-02-08 13:51:36 +0000

    Outline which fields to be returned by serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,4 +5,25 @@ from .models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    '''
+    What do I want to display to the user in a response?
+    author 
+    title 
+    description
+    dish_type 
+    difficulty 
+    time 
+    time_unit 
+    servings 
+    ingredients_list 
+    procedure
+    tags 
+    recipe_image 
+
+    Cacluated fields:
+    is_author
+    number of likes
+    likes_count
+    number of comments
+    '''
     pass
