2f5053b5562f134bd9e1ec719265d0e84f66c6f6
tony-albanese <tony.albanese2000@gmail.com>
2023-02-09 10:43:52 +0000

    Write test cases for RecipeView

--- recipes/tests.py
+++ recipes/tests.py
@@ -19,13 +19,32 @@ class RecipeViewTests(APITestCase):
             procedure='{}',
             tags=""
         )
-    
+          
     def test_user_can_get_list_of_recipes(self):
         response = self.client.get('/recipes/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_logged_in_user_can_create_recipe(self):
-        pass
+        recipe_data = {
+            'title': 'test title 2',
+            'description': 'a description',
+            'ingredients_list': '{}',
+            'procedure': '{}',
+            'tags': "tag"
+        }
+        self.client.login(username='test_user', password='password')
+        response = self.client.post('/recipes/', recipe_data)
+        count = Recipe.objects.count()
+        self.assertEqual(count, 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_anonymous_user_cant_create_recipe(self):
-        pass
+        recipe_data = {
+            'title': 'test title 2',
+            'description': 'a description',
+            'ingredients_list': '{}',
+            'procedure': '{}',
+            'tags': "tag"
+        }
+        response = self.client.post('/recipes/', recipe_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
