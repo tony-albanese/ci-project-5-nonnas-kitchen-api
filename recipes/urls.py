40da36cd512b21a6bb11b2f2194bbe69e9ae5f20
tony-albanese <tony.albanese2000@gmail.com>
2023-02-11 13:55:28 +0000

    Add missing slash at end of url.

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('recipes/', views.RecipeView.as_view()),
     path('recipes/<int:pk>/', views.RecipeDetailView.as_view()),
     path('recipes/likes/', views.RecipeLikeList.as_view()),
-    path('recipes/likes/<int:pk>', views.RecipeLikeDetailView.as_view()),
+    path('recipes/likes/<int:pk>/', views.RecipeLikeDetailView.as_view()),
 ]
