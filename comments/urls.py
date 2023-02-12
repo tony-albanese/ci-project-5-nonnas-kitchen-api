2695652a9338010ae56c8eff493899f78d0c1e54
tony-albanese <tony.albanese2000@gmail.com>
2023-02-12 09:57:48 +0000

    Add url to map to recipe comment endpoint

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,6 @@ from comments import views
 
 urlpatterns = [
     path('comments/', views.CommentList.as_view()),
-    path('comments/<int:pk>/', views.CommentDetail.as_view())
+    path('comments/<int:pk>/', views.CommentDetail.as_view()),
+    path('recipes/comments/', views.RecipeCommentList.as_view())
 ]
