a91541cccf10176175a4ff70749df0b7fbde76ef
tony-albanese <tony.albanese2000@gmail.com>
2023-02-12 09:24:00 +0000

    Outline test cases for RecipeComment enndpoint

--- comments/tests.py
+++ comments/tests.py
@@ -1,9 +1,11 @@
-from .models import Comment, BlogPost
+from .models import Comment, BlogPost, RecipeComment, Recipe
 from kitchen_user.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 # Create your tests here.
+
+
 class CommentViewTests(APITestCase):
     def setUp(self):
         User.objects.create_user(username='test_user', password='password')
@@ -72,3 +74,55 @@ class CommentViewTests(APITestCase):
         response = self.client.delete('/comments/1/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+
+class RecipeCommentViewTests(APITestCase):
+    def settings(self):
+        User.objects.create_user(username='test_user', password='password')
+        user_a = User.objects.create_user(username='user_a', password='pass')
+        user_b = User.objects.create_user(username='user_b', password='pass')
+
+        # Create some recipes
+        Recipe.objects.create(
+            author=user_a,
+            title='title 1',
+            description='description 1',
+            ingredients_list='{}',
+            procedure='{}',
+            tags=""
+        )
+
+        Recipe.objects.create(
+            author=user_a,
+            title='title 2',
+            description='description 2',
+            ingredients_list='{}',
+            procedure='{}',
+            tags=""
+        )
+        # Create some commments.
+        recipe_a = Recipe.objects.get(pk=1)
+        recipe_b = Recipe.objects.get(pk=2)
+
+        RecipeComment.objects.create(author=user_a, recipe=recipe_a, body="recipe comment A")
+        RecipeComment.objects.create(author=user_a, recipe=recipe_b, body="recipe comment B")
+
+    def test_view_all_comments(self):
+        pass
+
+    def test_can_create_comment_if_logged_in(self):
+        pass
+
+    def test_cannot_create_comment_if_anonymous(self):
+        pass
+
+    def test_user_can_update_their_own_comment(self):
+        pass
+
+    def test_user_cannot_update_comment_if_not_owner(self):
+        pass
+
+    def test_user_can_delete_their_own_comment(self):
+        pass
+
+    def test_user_cannot_delete_comment_if_not_owner(self):
+        pass
