085db9d2d7d0a7ef4426abaf76a9f0eca622b3a0
tony-albanese <tony.albanese2000@gmail.com>
2023-02-13 14:23:07 +0000

    Put ALLOWED_HOST back into settings.py

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -64,8 +64,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
     'localhost', 
-    'nonnas-kitchen-api.herokuapp.com'
     ]
 
 
