76879583fccf311989c52ec3d1980b8f4ee19e42
tony-albanese <tony.albanese2000@gmail.com>
2023-02-18 10:34:30 +0000

    Changed CATEGORIES to list to stop migrations in BlogPost model.

--- posts/models.py
+++ posts/models.py
@@ -6,14 +6,14 @@ from taggit.managers import TaggableManager
 
 
 class BlogPost(models.Model):
-    CATEGORIES = {
+    CATEGORIES = [
         ('anec', 'Anecdote'),
         ('tip', 'Tip'),
         ('hist', 'History'),
         ('fact', 'Fun Fact'),
         ('orig', 'Origin'),
         ('remin', 'Reminiscence')
-    }
+    ]
 
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_author')
